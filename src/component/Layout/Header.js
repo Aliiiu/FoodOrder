54814da18a38f7d88748ad7285255ead3e0e8c97
aliiiu <ayokunumi99@gmail.com>
2022-02-26 20:49:34 +0100

    Title Update

--- src/component/Layout/Header.js
+++ src/component/Layout/Header.js
@@ -6,7 +6,7 @@ const Header = (props) => {
     return (
         <Fragment>
             <header className= { classes.header }>
-                <h1> React Meals </h1>
+                <h1> Tasty Meals </h1>
                 <HeaderCardButton onClick={ props.onShowCart }/>
             </header>
             <div className={classes['main-image']}>
